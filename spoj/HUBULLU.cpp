d249fc39387bcb32a55f6013ae76504251038465
Manuelacosta98 <33484814+Manuelacosta98@users.noreply.github.com>
2017-11-08 08:39:51 -0400

    Create HUBULLU.cpp

---
+++ spoj/HUBULLU.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+	int t,n,m;
+	scanf("%d",&t);
+	while(t--)
+	{
+		scanf("%d %d",&n,&m);
+		(m)?printf("Pagfloyd wins.\n"):printf("Airborne wins.\n");;
+	}
+	return 0;
+} 
